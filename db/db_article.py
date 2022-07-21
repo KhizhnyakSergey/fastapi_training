9a4d15ae75795d54bc713eca72fb9ad8ed204d59
KhizhnyakSergey <Khihnyak56843@gmail.com>
2022-07-21 23:28:55 +0300

    build: covered topics such as: error, response, headers, cookies, form and cors

--- db/db_article.py
+++ db/db_article.py
@@ -1,9 +1,13 @@
 from db.models import DbArticle
+from exeptions import StoryExeption
 from schemas import ArticleBase
 from sqlalchemy.orm.session import Session
+from fastapi import HTTPException, status
 
 
 def create_article(db: Session, request: ArticleBase):
+    if request.content.startswith('Once upon a time'):
+        raise StoryExeption('No stories please')
     new_article = DbArticle(
         title = request.title,
         content = request.content,
@@ -17,5 +21,7 @@ def create_article(db: Session, request: ArticleBase):
 
 def get_article(db: Session, id: int):
     article = db.query(DbArticle).filter(DbArticle.id == id).first()
-    # Handle errors
+    if not article:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'Article with id {id} not found')
     return article
---
+++ exeptions.py
@@ -0,0 +1,3 @@
+class StoryExeption(Exception):
+    def __init__(self, name: str):
+        self.name = name
