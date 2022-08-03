b74b83b7ea27dd5afd0caf5f8fd7703d269357ec
KhizhnyakSergey <Khihnyak56843@gmail.com>
2022-08-03 20:44:21 +0300

    feat: async await, templates, middleware, web sockets

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+import imp
 from unicodedata import name
 from urllib.request import Request
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, WebSocket
 from exeptions import StoryExeption
 from router import blog_get, blog_post, user, article, product, file
 from auth import authentication
+from templates import templates
 from db import models
 from db.database import engine
 from fastapi import Request
-from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.responses import JSONResponse, PlainTextResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+import time
+from client import html
+from fastapi.websockets import WebSocket
 
 app = FastAPI()
+app.include_router(templates.router)
 app.include_router(authentication.router)
 app.include_router(file.router)
 app.include_router(user.router)
@@ -32,11 +38,37 @@ def story_exeption_handler(request: Request, exc: StoryExeption):
         content={'detail': exc.name}
     )
 
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+clients = []
+
+@app.websocket("/chat")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    while True:
+        data = await websocket.receive_text()
+        for client in clients:
+            await client.send_text(data)
+
+
+
 # @app.exception_handler(HTTPException)
 # def custom_handler(request: Request, exc: StoryExeption):
 #     return PlainTextResponse(str(exc), status_code=400)
 
 models.Base.metadata.create_all(engine)
+
+@app.middleware("http")
+async def add_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration =time.time() - start_time
+    response.headers['duration'] = str(duration)
+    return response
 
 origins = [
     'http://localhost:3000'
@@ -51,3 +83,7 @@ app.add_middleware(
 )
 
 app.mount('/files', StaticFiles(directory="files"), name='files')
+app.mount('/templates/static', 
+    StaticFiles(directory="templates/static"),
+    name="static"
+)
